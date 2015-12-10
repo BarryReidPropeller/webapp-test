7613cf1ecae88c7561deed51f93069c4ce3f59fa
barryreidpropeller <barry@propelleraero.com.au>
2015-12-10 11:13:43 +1100

    test

--- circle/circle-deploy-production.sh
+++ circle/circle-deploy-production.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 PROFILE=${AWS_PROFILE:-$DEFAULT}
-BUCKET=development.prpeller.com
+BUCKET=production.prpeller.com.s3.amazonaws.com
 DIR=./
 aws  s3  sync $DIR s3://$BUCKET/ --exclude ".git/*" --exclude "circle/*" --exclude "test/*"
